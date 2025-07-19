11273baafbfae147e58b3d97786585e177dcee37
SavEvens <savannaevens@sevensprojects.net>
2025-07-19 00:10:05 -0500

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,93 @@
 use std::ops::{Index, IndexMut, Range};
 
-enum ExclusiveRangeMapNodeColor{
+enum ExclusiveRangeMapNodeColor {
     RED,
-    BLACK
+    BLACK,
 }
 
-struct ExclusiveRangeMapNode<T>{
-    range : Range<usize>,
-    value : T,
-    color : ExclusiveRangeMapNodeColor,
-    child_left  : Option<Box<ExclusiveRangeMapNode<T>>>,
-    child_right : Option<Box<ExclusiveRangeMapNode<T>>>
+struct ExclusiveRangeMapNode<T> {
+    range: Range<usize>,
+    value: T,
+    color: ExclusiveRangeMapNodeColor,
+    child_left: Option<Box<ExclusiveRangeMapNode<T>>>,
+    child_right: Option<Box<ExclusiveRangeMapNode<T>>>,
 }
-
 
 pub struct ExclusiveRangeInvalidInsertError;
 
-enum ExclusiveRangeMapNodeRotateDirection{
+enum ExclusiveRangeMapNodeRotateDirection {
     RIGHT,
-    LEFT
+    LEFT,
 }
 
-impl<T> ExclusiveRangeMapNode<T>{
-
-    fn new(range : Range<usize>, value : T) -> ExclusiveRangeMapNode<T>{
-        ExclusiveRangeMapNode{
+impl<T> ExclusiveRangeMapNode<T> {
+    fn new(range: Range<usize>, value: T) -> ExclusiveRangeMapNode<T> {
+        ExclusiveRangeMapNode {
             range: range,
             value: value,
             color: ExclusiveRangeMapNodeColor::BLACK,
             child_left: None,
-            child_right: None
+            child_right: None,
         }
     }
-
-
 }
 
-pub struct ExclusiveRangeMap<T>{
-    head : Option<Box<ExclusiveRangeMapNode<T>>>,
-    size : usize,
+pub struct ExclusiveRangeMap<T> {
+    head: Option<Box<ExclusiveRangeMapNode<T>>>,
+    size: usize,
 }
 
-
-impl<T> ExclusiveRangeMap<T>{
-    pub fn new() -> ExclusiveRangeMap<T>{
-        ExclusiveRangeMap{
+impl<T> ExclusiveRangeMap<T> {
+    pub fn new() -> ExclusiveRangeMap<T> {
+        ExclusiveRangeMap {
             head: None,
-            size: 0
+            size: 0,
         }
     }
 
-    pub fn len(&self) -> usize{
+    pub fn len(&self) -> usize {
         self.size
     }
 
-    fn place_node(start_node : &mut ExclusiveRangeMapNode<T>, new_node : ExclusiveRangeMapNode<T>) -> Result<(), ExclusiveRangeInvalidInsertError> {
+    fn place_node(
+        start_node: &mut ExclusiveRangeMapNode<T>,
+        new_node: ExclusiveRangeMapNode<T>,
+    ) -> Result<(), ExclusiveRangeInvalidInsertError> {
         let selected_node = start_node;
-        loop{
-            
+        loop {
             let left = selected_node.range.start > new_node.range.end;
             let right = selected_node.range.end < new_node.range.start;
 
-            if left && right{
+            if left && right {
                 return Err(ExclusiveRangeInvalidInsertError);
             }
 
-            if new_node.range.start < selected_node.range.start 
-                && new_node.range.end > selected_node.range.start {
+            if new_node.range.start < selected_node.range.start
+                && new_node.range.end > selected_node.range.start
+            {
                 return Err(ExclusiveRangeInvalidInsertError);
             }
             if new_node.range.start < selected_node.range.end
-                && new_node.range.end > selected_node.range.end {
+                && new_node.range.end > selected_node.range.end
+            {
                 return Err(ExclusiveRangeInvalidInsertError);
             }
 
-
-            if left{
-                match &mut selected_node.child_left{
-                    Some(child) => {
-        return ExclusiveRangeMap::place_node(child, new_node);
-                    },
-                    None => {
-                        selected_node.child_right = Some(Box::new(new_node));
-                        return Ok(())
-                    }
-                }
-            }else{
-                match &mut selected_node.child_right{
+            if left {
+                match &mut selected_node.child_left {
                     Some(child) => {
                         return ExclusiveRangeMap::place_node(child, new_node);
-                    },
+                    }
+                    None => {
+                        selected_node.child_right = Some(Box::new(new_node));
+                        return Ok(());
+                    }
+                }
+            } else {
+                match &mut selected_node.child_right {
+                    Some(child) => {
+                        return ExclusiveRangeMap::place_node(child, new_node);
+                    }
                     None => {
                         selected_node.child_right = Some(Box::new(new_node));
                         return Ok(());
@@ -97,20 +95,24 @@ impl<T> ExclusiveRangeMap<T>{
                 }
             }
         }
-
     }
 
     fn balance_tree(&mut self) -> () {
         //TODO
     }
 
-    pub fn insert(&mut self, key : Range<usize>, value : T)
-        -> Result<(), ExclusiveRangeInvalidInsertError>{
-        match &mut self.head{
+    pub fn insert(
+        &mut self,
+        key: Range<usize>,
+        value: T,
+    ) -> Result<(), ExclusiveRangeInvalidInsertError> {
+        match &mut self.head {
             Some(head_node) => {
-                let res = ExclusiveRangeMap::place_node(head_node,
-                    ExclusiveRangeMapNode::new(key, value));
-                match res{
+                let res = ExclusiveRangeMap::place_node(
+                    head_node,
+                    ExclusiveRangeMapNode::new(key, value),
+                );
+                match res {
                     Ok(_) => {
                         self.balance_tree();
                         self.size += 1;
@@ -118,45 +120,45 @@ impl<T> ExclusiveRangeMap<T>{
                     Err(_) => {}
                 }
                 return res;
-            },
-            None=> {
+            }
+            None => {
                 self.head = Some(Box::new(ExclusiveRangeMapNode::new(key, value)));
                 self.size += 1;
-                return Ok(())
+                return Ok(());
             }
         }
     }
 
-    fn find_node(&self, key : &usize) -> Option<&Box<ExclusiveRangeMapNode<T>>> {
-        match & self.head{
-            Some(_) => {},
+    fn find_node(&self, key: &usize) -> Option<&Box<ExclusiveRangeMapNode<T>>> {
+        match &self.head {
+            Some(_) => {}
             None => {
                 return None;
             }
         }
-        let mut selected_node : &Box<ExclusiveRangeMapNode<T>>;
+        let mut selected_node: &Box<ExclusiveRangeMapNode<T>>;
         selected_node = self.head.as_ref()?;
 
-        loop{
+        loop {
             let left = selected_node.range.start > *key;
             let right = selected_node.range.end <= *key;
-            if left{
-                match selected_node.child_left{
+            if left {
+                match selected_node.child_left {
                     Some(_) => {
                         selected_node = selected_node.child_left.as_ref()?;
                         continue;
-                    },
+                    }
                     None => {
                         return None;
                     }
                 }
             }
-            if right{
-                match selected_node.child_right{
+            if right {
+                match selected_node.child_right {
                     Some(_) => {
                         selected_node = selected_node.child_right.as_ref()?;
                         continue;
-                    },
+                    }
                     None => {
                         return None;
                     }
@@ -165,57 +167,55 @@ impl<T> ExclusiveRangeMap<T>{
 
             return Some(selected_node);
         }
-
-        
     }
 
-    pub fn find(&self, key : &usize) -> Option<&T> {
+    pub fn find(&self, key: &usize) -> Option<&T> {
         match self.find_node(&key) {
-            Some(node) => {
-                Some(&node.value)
-            },
-            None => {
-                None
-            }
+            Some(node) => Some(&node.value),
+            None => None,
         }
     }
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_create_rangepoint_map(){
+    fn test_create_rangepoint_map() {
         let new_map = ExclusiveRangeMap::<u64>::new();
 
         assert_eq!(new_map.size, 0);
     }
 
     #[test]
-    fn test_basic_rangepoint_map_ops(){
+    fn test_basic_rangepoint_map_ops() {
         let mut map = ExclusiveRangeMap::<u64>::new();
 
         let res1 = map.insert(0..5 as usize, 12);
         let res2 = map.insert(6..12 as usize, 64);
         let res3 = map.insert(14..55 as usize, 1);
 
-        match res1{
-            Ok(()) => {},
-            Err(_) => {panic!("This insert should pass!");}
-        }
-        
-        match res2{
-            Ok(()) => {},
-            Err(_) => {panic!("This insert should pass!");}
+        match res1 {
+            Ok(()) => {}
+            Err(_) => {
+                panic!("This insert should pass!");
+            }
         }
 
-        match res3{
-            Ok(()) => {},
-            Err(_) => {panic!("This insert should pass!");}
+        match res2 {
+            Ok(()) => {}
+            Err(_) => {
+                panic!("This insert should pass!");
+            }
         }
 
+        match res3 {
+            Ok(()) => {}
+            Err(_) => {
+                panic!("This insert should pass!");
+            }
+        }
 
         assert_eq!(map.size, 3);
 
@@ -237,34 +237,33 @@ mod tests{
         assert_eq!(err3, None);
         assert_eq!(err4, None);
     }
-    
-    #[test]
-    fn test_invalid_rangepoint_map_insert(){
 
+    #[test]
+    fn test_invalid_rangepoint_map_insert() {
         let mut map = ExclusiveRangeMap::<u64>::new();
 
         let res1 = map.insert(0..20 as usize, 44);
         let res2 = map.insert(20..50 as usize, 55);
         let res3 = map.insert(45..60 as usize, 77);
 
-        match res1{
-            Ok(()) => {},
+        match res1 {
+            Ok(()) => {}
             Err(ExclusiveRangeInvalidInsertError) => {
                 panic!("There should be no error in this insertion.");
             }
         }
 
-        match res2{
-            Ok(()) => {},
+        match res2 {
+            Ok(()) => {}
             Err(ExclusiveRangeInvalidInsertError) => {
                 panic!("There should be no error in this insertion.");
             }
         }
 
-        match res3{
+        match res3 {
             Ok(()) => {
                 panic!("This insertion should cause an error!");
-            },
+            }
             Err(ExclusiveRangeInvalidInsertError) => {}
         }
     }
